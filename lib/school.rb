c813c22828b2e0b74ffa77ae4975e08bdabe43d1
Sharon Watkins <swatkins876@gmail.com>
2019-09-21 19:49:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |a, b| 
+      if a == level
+        return y 
+      end 
+    end 
